2866e68fe8f2497132e973038d0c19e23eaf1bea
Mikhail Petrov <mehael@mail.ru>
2020-10-26 00:14:50 +1000

    Add WorldToCanvas coords transition.

--- ExtensionMethods.cs
+++ ExtensionMethods.cs
@@ -105,4 +105,15 @@ public static class ExtensionMethods
     {
         return new Vector3(vector2.x, vector2.y, 0);
     }
+    
+    public static Vector2 WorldToCanvas(this Canvas canvas, Vector3 worldPosition)
+    {
+        var camera = Camera.main;
+        var viewportPosition = camera.WorldToViewportPoint(worldPosition);
+        var canvasRect = canvas.GetComponent<RectTransform>();
+
+        var sizeDelta = canvasRect.sizeDelta;
+        return new Vector2((viewportPosition.x * sizeDelta.x) - (sizeDelta.x * 0.5f),
+            (viewportPosition.y * sizeDelta.y) - (sizeDelta.y * 0.5f));
+    }
 }
