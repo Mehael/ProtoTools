bf2fc68c6bb26ce2b43f67d8d93b51186dba94e6
Mikhail Petrov <mehael@mail.ru>
2020-12-03 17:07:42 +1000

    Fix rigidbody requirement.

--- ConstantMovement.cs
+++ ConstantMovement.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class ConstantMovement : MonoBehaviour
 {
     private Rigidbody2D rBody;
