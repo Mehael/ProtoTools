40c6ae40428f34818dfd732d0e8dfb3ec32a7639
Mikhail Petrov <mehael@mail.ru>
2021-02-26 12:23:55 +1000

    Add force serelize btn

---
+++ Reserilizer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+ 
+namespace EditorExtensions.MenuItems {
+    public static class ReserializeAssets {
+        [MenuItem("Tools/Force Re-Serialize Assets")]
+        public static void ForceReSerializeAssets() {
+            AssetDatabase.ForceReserializeAssets();
+        }
+    }
+}
